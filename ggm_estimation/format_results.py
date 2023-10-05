6ebea9922ed9ea46df027be14c7efc4b037e8563
Tenceto <martin_s8@hotmail.com>
2023-10-05 15:22:23 -0500

    Additional experiments

--- ggm_estimation/format_results.py
+++ ggm_estimation/format_results.py
@@ -25,24 +25,28 @@ def compute_estimation_performance(filename, tuneable_methods, fixed_methods, tr
     for col in ["real_values"] + [f"pred_{method}" for method in tuneable_methods + fixed_methods]:
         df[col] = df[col].apply(lambda x: np.array(eval(x)))
 
-    seeds = df["seed"].unique()
-    print("Samples:", len(seeds))
-    final_scores = 0
+    if len(tuneable_methods) != 0 and train_size is not None and threshold_grid is not None and n_splits is not None:
+        seeds = df["seed"].unique()
+        print("Samples:", len(seeds))
+        final_scores = 0
 
-    for split in range(n_splits):
-        train_seeds = np.random.choice(seeds, int(train_size * len(seeds)), replace=False)
-        df_train = df[df["seed"].isin(train_seeds)]
-        df_test = df[~ df["seed"].isin(train_seeds)]
-        # print("Training samples:", df_train["seed"].nunique())
-        # print("Test samples:", df_test["seed"].nunique())
+        for split in range(n_splits):
+            train_seeds = np.random.choice(seeds, int(train_size * len(seeds)), replace=False)
+            df_train = df[df["seed"].isin(train_seeds)]
+            df_test = df[~ df["seed"].isin(train_seeds)]
+            # print("Training samples:", df_train["seed"].nunique())
+            # print("Test samples:", df_test["seed"].nunique())
 
-        final_scores = final_scores + _select_threshold_validation_set(df_train, df_test, tuneable_methods, threshold_grid, metric_fun, col_x_axis)
+            final_scores = final_scores + _select_threshold_validation_set(df_train, df_test, tuneable_methods, threshold_grid, metric_fun, col_x_axis)
+        
+        final_scores = (final_scores / n_splits).to_dict()
     
-    final_scores = (final_scores / n_splits).to_dict()
+    if 'final_scores' not in locals():
+        final_scores = dict()
 
     for method in fixed_methods:
-        df_test['metric'] = df_test.apply(lambda row: metric_fun(row["real_values"], row[f"pred_{method}"]), axis=1)
-        final_scores[method] = df_test.groupby(col_x_axis)['metric'].mean().to_dict()
+        df['metric'] = df.apply(lambda row: metric_fun(row["real_values"], row[f"pred_{method}"]), axis=1)
+        final_scores[method] = df.groupby(col_x_axis)['metric'].mean().to_dict()
     
     return final_scores
 
@@ -68,9 +72,11 @@ def _select_threshold_validation_set(df_train, df_test, tuneable_methods, thresh
 
 
 def plot_results(accuracy_dict, labels, title="", output_file=None, colors=None,
-                 linestyles=None, xlabel=r"Number of observations $k$", ylabel="Edge prediction accuracy", logscale=True,
+                 linestyles=None, xlabel=r"$k$", ylabel="Edge prediction accuracy", logscale=True,
                  legend_loc="best", ylims=None, legend_ncol=1):
-    marker_list = ['o', 'v', '^', 's', 'p', 'x', '+']
+    
+    marker_list = ['o', 'v', '^', 's', 'p', 'x', '+', 'P', 'd']
+    
     fig, ax = plt.subplots()
     if linestyles is None:
         linestyles = {method: "-" for method in accuracy_dict.keys()}
@@ -99,27 +105,35 @@ def plot_results(accuracy_dict, labels, title="", output_file=None, colors=None,
         plt.show()
 
 
-def plot_glasso_tuning(filename, graph_type=None, nans=None, n_proportional=None, one_zero_ratio=None):
+def plot_glasso_tuning(filename, graph_type=None, nans=None, 
+                       n_proportional=None, one_zero_ratio=None, metric="Accuracy",
+                       output_file=None):
     tuning_data = pd.read_csv(filename, sep=";").groupby(["num_obs", "lambda"])["metric"].mean().to_frame()
     num_obs_list = tuning_data.index.get_level_values("num_obs").unique()
     max_per_num_obs = tuning_data.groupby(level=0)['metric'].idxmax().apply(lambda x: x[1])
 
     popt, _ = curve_fit(_lambda_generic, max_per_num_obs.index, max_per_num_obs.values)
 
-    _, axs = plt.subplots(1, 2, figsize=(12, 4))
+    _, axs = plt.subplots(1, 2, figsize=(10, 4))
     for num_obs in num_obs_list:
         axs[0].plot(tuning_data.loc[num_obs]["metric"].index, tuning_data.loc[num_obs]["metric"].values, label=rf"$k = {num_obs}$")
     axs[1].plot(max_per_num_obs.index, max_per_num_obs.values, marker="o", label="Data")
-    axs[1].plot(num_obs_list, _lambda_generic(num_obs_list, *popt), label="New fit")
-    if graph_type is not None and nans is not None and n_proportional is not None and one_zero_ratio is not None:
-        axs[1].plot(num_obs_list, lambda_glasso_selector(graph_type, nans, n_proportional, one_zero_ratio)(num_obs_list).values, "--", label="Stored fit")
+    axs[1].plot(num_obs_list, _lambda_generic(num_obs_list, *popt), label="Fitted curve")
+    # if graph_type is not None and nans is not None and n_proportional is not None and one_zero_ratio is not None:
+    #     axs[1].plot(num_obs_list, lambda_glasso_selector(graph_type, nans, n_proportional, one_zero_ratio)(num_obs_list).values, "--", label="Tuned fit")
     axs[0].set_xscale("log")
     axs[0].set_xlabel(r"$\lambda$")
-    axs[0].set_ylabel(f"Metric")
+    axs[0].set_ylabel(metric)
     axs[0].legend()
+    axs[0].grid()
     axs[1].set_xscale("log")
     axs[1].set_xlabel(r"$k$")
     axs[1].set_ylabel(r"Optimal $\lambda$")
     axs[1].legend()
+    axs[1].grid()
     print(popt)
-    plt.show()
+    if output_file is not None:
+        plt.savefig(output_file, bbox_inches="tight")
+    else:
+        plt.show()
+
