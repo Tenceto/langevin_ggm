005b7d4784d24c7d3c6273749969c9c1046acafe
Tenceto <martin_s8@hotmail.com>
2024-05-25 14:38:08 +0000

    Refactor utils module

--- utils/ggm_utils.py
+++ utils/ggm_utils.py
@@ -1,19 +1,5 @@
 import numpy as np
-import torch
 
-
-def adj_matrix_to_vec(H):
-    h = H[np.triu_indices_from(H, k=1)]
-    return h
-
-def vec_to_adj_matrix(h, output_size=None):
-    if output_size is None:
-        output_size = (1 + np.sqrt(1 + 8 * len(h))) / 2
-        output_size = int(output_size)
-    H = np.zeros((output_size, output_size))
-    H[np.triu_indices_from(H, k=1)] = h
-    H = H + H.T
-    return H
 
 def random_nan_replacement(x, n_choose, n_proportional=True, one_zero_ratio=None):
     x = x.astype(float)
@@ -44,17 +30,6 @@ def random_nan_replacement(x, n_choose, n_proportional=True, one_zero_ratio=None
     x[mask_1] = np.nan
 
     return x
-
-def pad_adjs(ori_adj, node_number):
-    a = ori_adj
-    ori_len = a.shape[-1]
-    if ori_len == node_number:
-        return a
-    if ori_len > node_number:
-        raise ValueError(f'ori_len {ori_len} > node_number {node_number}')
-    a = torch.concatenate([a, torch.zeros([ori_len, node_number - ori_len])], axis=-1)
-    a = torch.concatenate([a, torch.zeros([node_number - ori_len, node_number])], axis=0)
-    return a
 
 def _lambda_generic(n, a, b, c):
 	return 1 / (a * np.log(n) ** 2 + b * np.log(n) + c)
