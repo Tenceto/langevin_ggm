646ff82f510b4b14fe4b411510377f2714516781
Tenceto <martin_s8@hotmail.com>
2024-05-26 14:17:17 +0000

    Script with bootstrap

--- main_bootstrap.py
+++ main_bootstrap.py
@@ -3,135 +3,90 @@ import numpy as np
 import pandas as pd
 import torch
 import logging
-from scipy.stats import wishart
 from functools import partial
 from contextlib import redirect_stdout
-# import warnings
+from argparse import ArgumentParser
+import yaml
+from easydict import EasyDict as edict
+from datetime import datetime
+import pprint
 
 import ggm_estimation.predictors as ggmp
 import ggm_estimation.data_generation as gen
-import utils.ggm_utils as ut
-from utils.torch_load import load_model, score_edp_wrapper
+from ggm_estimation import priors
+import utils.ggm_utils as ggm_utils
+from utils.torch_load import load_edpgnn_from_ckpt, score_fun_edpgnn
 
 torch.set_default_device("cuda")
 torch.set_default_dtype(torch.float32)
-# warnings.filterwarnings("error")
 
-graph_type = "deezer"
-logger_file = f"langevin_ggm_{graph_type}.log"
 
-# Settings
+def main(config, logger, output_file):
+	logger.info(f"Running bootstrap simulations for {config.score_model.model_file}.")
+	logger.info(f"Config parameters used: \n{pprint.pformat(config)}")
 
-# GMRF parameters
-obs_ratio_list = [1.8, 3.2, 5.6, 10.0]
-prior_Theta = lambda num_nodes: wishart(num_nodes, np.eye(num_nodes) * 10 / num_nodes)
+	# Read parameters from config file
 
-# Simulation parameters
-n_sim = 100
-seed = 123456
-psd_trials = 10
-margins = [0.2, 0.3, 0.4, 0.45]
-n_bootstrap = 50
+	# General parameters
+	device = config.device
 
-# Graph parameters
-if graph_type == "grids":
-	# Graph parameters
-	m_min = 5
-	m_max = 9
-	min_nodes = 40
-	max_nodes = 49
-	min_random_edges = 2
-	max_random_edges = 5
-	# Prior model
-	model_file = ("scorematching_gnn/exp/grids_dif_nodes/edp-gnn_grids_dif_nodes__Feb-12-22-48-21_4158697/models/" + 
-				  "grids_dif_nodes_[0.03, 0.08222222, 0.13444444, 0.18666667, 0.23888889, 0.29111111, 0.34333333, 0.39555556, 0.44777778, 0.5].pth")
-	# Graph creation function
-	graph_generator = partial(gen.generate_grids, 
-						   	  m_min=m_min, m_max=m_max, min_nodes=min_nodes, 
-						   	  max_nodes=max_nodes, min_random_edges=min_random_edges, 
-						   	  max_random_edges=max_random_edges, seed=seed)
-elif graph_type == "barabasi":
-	# Graph parameters
-	nodes_list = [46, 48, 50, 52]
-	m1 = 2
-	m2 = 4
-	p = 0.5
-	max_nodes = 53
-	# Prior model
-	model_file = ("scorematching_gnn/exp/barabasi_albert_diff_nodes/edp-gnn_barabasi_albert_[47, 49, 51, 53]__Jun-13-10-13-20_999031/models/" +
-				  "barabasi_albert_[47, 49, 51, 53]_[0.03, 0.08222222, 0.13444444, 0.18666667, 0.23888889, 0.29111111, 0.34333333, 0.39555556, 0.44777778, 0.5].pth")
-	# Graph creation function
-	graph_generator = partial(gen.generate_barabasi_albert, 
-						   	  nodes_list=nodes_list, m1=m1, m2=m2, p=p, seed=seed)
-elif graph_type == "deezer":
-	# Graph parameters
-	max_nodes = 25
-	# Prior model
-	model_file = ("scorematching_gnn/exp/deezer_ego/edp-gnn_train_deezer_ego__Jun-14-14-14-11_1489048/models/" +
-			      "train_deezer_ego_[0.03, 0.08222222, 0.13444444, 0.18666667, 0.23888889, 0.29111111, 0.34333333, 0.39555556, 0.44777778, 0.5].pth")
-	# Graph creation function
-	graph_generator = partial(gen.load_graph_dataset, filename="scorematching_gnn/data/test_deezer_ego.pkl")
+	# Score model parameters
+	model_file = f"./trained_networks/{config.score_model.model_file}.pth"
+	sigmas = np.linspace(config.score_model.sigmas.max, config.score_model.sigmas.min, config.score_model.sigmas.num)
+	max_nodes = config.score_model.max_nodes
+	epsilon = config.score_model.epsilon
+	steps = config.score_model.steps
+	temperature = config.score_model.temperature
+	num_samples = config.score_model.num_samples
 
-# Langevin predictor parameters
-sigmas = np.linspace(0.5, 0.03, 10)
-epsilon = 1.0E-6
-steps = 300
-temperature = 1.0
-num_samples = 10
+	# GGM simulation parameters
+	obs_ratio_list = config.ggm_simulation.obs_ratio_list
+	prior_Theta = getattr(priors, config.ggm_simulation.prior_Theta)
+	n_proportional = config.ggm_simulation.n_proportional
+	nans = config.ggm_simulation.nans
+	psd_trials = config.ggm_simulation.psd_trials
+	try:
+		one_zero_ratio = config.ggm_simulation.one_zero_ratio
+	except:
+		one_zero_ratio = None
+	generator_fun = getattr(gen, config.graph_generation.method)
 
-# Output file
-output_file = f"outputs/full_{seed}_{graph_type}_{num_samples}_{margins}_{n_bootstrap}.csv"
-
-# Lambda function
-lambda_fun = ut.lambda_glasso_selector(graph_type, nans=1.0, nans_proportional=True, one_zero_ratio=None)
-
-if __name__ == '__main__':
-	logging.basicConfig(filename=f"logs/{logger_file}",
-						filemode='a',
-						format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-						datefmt='%H:%M:%S',
-						level=logging.DEBUG)
-	logger = logging.getLogger('Langevin')
-	logger.info("Running Langevin Simulation")
-
-	def exc_handler(exctype, value, tb):
-		logger.exception(''.join(traceback.format_exception(exctype, value, tb)))
-
-	# Install exception handler
-	sys.excepthook = exc_handler
-
-	model = load_model(model_file)
+	# GLasso parameters
+	lambda_fun = partial(getattr(ggm_utils, config.lambda_glasso.fun), **config.lambda_glasso.params)
 	
-	simulation_args = [(seed, g, logger) for seed, g in enumerate(graph_generator(n_sim=n_sim))]
-
-	logger.info(f"Matrices generated.")
-
+	# Define all estimators to be used
+	model = load_edpgnn_from_ckpt(model_file, device=device)
 	quic = ggmp.QuicEstimator(lambda_fun)
-	stability_selector = ggmp.StabilitySelector(n_bootstrap, lambda_fun, mode="manual", n_jobs=1)
-
-	def simulation_wrapper(args):
-		seed, A, logger = args
-		num_nodes = A.shape[0]
+	stability_selector = ggmp.StabilitySelector(config.bootstrap.n_bootstrap, lambda_fun, mode="manual", 
+												n_jobs=config.n_jobs)
+	
+	# Start the simulations
+	for seed, A in enumerate(generator_fun(**config.graph_generation.params)):
 		output_results = list()
 		np.random.seed(seed)
 
-		max_num_obs = int(np.ceil(obs_ratio_list[-1] * num_nodes))
+		n_missing = nans if n_proportional is False else np.ceil(nans * A.shape[0] * (A.shape[0] - 1) / 2)
+		max_num_obs = int(np.ceil(obs_ratio_list[-1] * n_missing))
 
-		_, X_obs = gen.simulate_ggm(A, max_num_obs, 
-									nans=1.0, one_zero_ratio=None, n_proportional=True, 
-									psd_trials=psd_trials, prior_Theta=prior_Theta, logger=logger)
+		try:
+			_, X_obs = gen.simulate_ggm(A, max_num_obs, nans, one_zero_ratio, n_proportional, 
+										psd_trials, prior_Theta, logger)
+		except gen.PSDGenerationError:
+			logger.warning(f"Could not generate a PSD matrix. Seed: {seed}")
+			continue
 
-		prior_A_score = score_edp_wrapper(model, num_nodes, len(sigmas), max_nodes)
-
+		prior_A_score = partial(score_fun_edpgnn, model=model, nodes=A.shape[0], 
+								sigmas=sigmas, max_nodes=max_nodes)
+		
 		langevin_posterior = ggmp.LangevinEstimator(sigmas=sigmas, epsilon=epsilon,
-											  		steps=steps, score_estimator=prior_A_score,
+													steps=steps, score_estimator=prior_A_score,
 													use_likelihood=True, use_prior=True)
-
+		
 		for obs_ratio in obs_ratio_list:
-			num_obs = int(np.ceil(obs_ratio * num_nodes))
+			num_obs = int(np.ceil(obs_ratio * n_missing))
 			# Estimate A_obs with bootstrapped glasso
 			A_stability_est = stability_selector.generate_sample(X_obs[:num_obs])
-
+			
 			A_all = {
 				"glasso": quic.generate_sample(None, X_obs[:num_obs]),
 				"stability": A_stability_est,
@@ -139,28 +94,28 @@ if __name__ == '__main__':
 
 			ratio_unknown = list()
 			# Langevin without stability selection
-			A_obs_est = torch.full(A.shape, torch.nan)
+			A_obs_est = torch.full(A.shape, torch.nan, device=device)
 			A_obs_est = A_obs_est.fill_diagonal_(0.0)
 			unknown_idx = torch.where(torch.isnan(torch.triu(A_obs_est, diagonal=1)))
 			A_langevin = langevin_posterior.generate_sample(A_obs_est, X_obs[:num_obs],
 															temperature=temperature, num_samples=num_samples,
-															seed=(seed + 1) * 3)
+															seed=(seed + 1) * 3, parallel=config.parallel, n_jobs=config.n_jobs)
 			A_all[f"langevin_flat"] = A_langevin
-			ratio = len(unknown_idx[0]) / (num_nodes * (num_nodes - 1) / 2)
+			ratio = len(unknown_idx[0]) / (A.shape[0] * (A.shape[0] - 1) / 2)
 			ratio_unknown.append(round(ratio, 2))
-			
+
 			# Langevin with stability selection
-			for margin in margins:
+			for margin in config.bootstrap.margins:
 				A_obs_est = stability_selector.threshold_probabilities(A_stability_est, margin=margin)
-				A_obs_est = torch.tensor(A_obs_est)
+				A_obs_est = torch.tensor(A_obs_est, device=device)
 				unknown_idx = torch.where(torch.isnan(torch.triu(A_obs_est, diagonal=1)))
 				A_langevin = langevin_posterior.generate_sample(A_obs_est, X_obs[:num_obs],
 															 	temperature=temperature, num_samples=num_samples,
-															 	seed=(seed + 1) * 3)
+															 	seed=(seed + 1) * 3, parallel=config.parallel, n_jobs=config.n_jobs)
 				A_all[f"langevin_{margin}"] = A_langevin
-				ratio = len(unknown_idx[0]) / (num_nodes * (num_nodes - 1) / 2)
+				ratio = len(unknown_idx[0]) / (A.shape[0] * (A.shape[0] - 1) / 2)
 				ratio_unknown.append(round(ratio, 2))
-			
+
 			triu_idx = np.triu_indices_from(A, k=1)
 
 			this_output = dict()
@@ -177,17 +132,36 @@ if __name__ == '__main__':
 				this_output[f"pred_{method}"] = values_sampled
 			output_results.append(this_output)
 
-			logger.info(f"Finished iteration. Seed: {seed}, k/n = {obs_ratio}, k = {num_obs}, n = {num_nodes}, |U| / dim(a): {ratio_unknown}")
+			logger.info(f"Finished iteration. Seed: {seed}, k/n = {obs_ratio}, k = {num_obs}, n = {A.shape[0]}, |U| / dim(a): {ratio_unknown}")
 		pd.DataFrame(output_results).to_csv(output_file, mode='a', sep=";", header=not os.path.exists(output_file))
 
-		return
+	return
+
+if __name__ == '__main__':
+	# Set up logging
+	logging.basicConfig(filename=f"logs/bootstrap_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S.log')}",
+						filemode='a',
+						format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+						datefmt='%H:%M:%S',
+						level=logging.DEBUG)
+	logging.captureWarnings(True)
+	logger = logging.getLogger('Langevin')
+
+	def exc_handler(exctype, value, tb):
+		logger.exception(''.join(traceback.format_exception(exctype, value, tb)))
+	sys.excepthook = exc_handler
+
+	# Read config file
+	parser = ArgumentParser()
+	parser.add_argument('-c','--config', help='Configuration file', required=True)
+	args = parser.parse_args()
+	config_dir = f'./config/{args.config}.yaml'
+	config = edict(yaml.load(open(config_dir, 'r'), Loader=yaml.FullLoader))
+
+	# Output filename
+	output_file = f"outputs/bootstrap_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.csv"
 
 	with redirect_stdout(logging):
-		for arg in simulation_args:
-			try:
-				simulation_wrapper(arg)
-			except RuntimeError:
-				logger.exception(f"Runtime error in iteration.")
-				continue
-
+		main(config, logger, output_file)
+	
 	logger.info(f"Finished all iterations.")
